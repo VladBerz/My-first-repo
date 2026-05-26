b29f5b4802b86d964dbe2972b7c7344e77b5844a
VladBerz <berzon.vladislav@gmail.com>
2026-05-26 16:23:23 +0300

    added todo app

--- app.py
+++ app.py
@@ -1,1 +1,24 @@
-print("Hello, world")
+tasks = []
+
+while True:
+    print("\nTODO LIST")
+    print("1. Show tasks")
+    print("2. Add tasks")
+    print("3. Exit")
+
+    choice = input("Choose option: ")
+
+    if choice == "1":
+        print(tasks)
+
+    elif choice == "2":
+        task = input("Enter task: ")
+        tasks.append(task)
+        print("Task added!")
+
+    elif choice == "3":
+        print("Goodbye!")
+        break
+
+    else:
+        print("Invalid option")    
