c18702f3ded537e2035b55a8d21a1e369c7dc3a6
VladBerz <berzon.vladislav@gmail.com>
2026-05-26 17:53:50 +0300

    added new command

--- app.py
+++ app.py
@@ -4,12 +4,22 @@ while True:
     print("\nTODO LIST")
     print("1. Show tasks")
     print("2. Add tasks")
-    print("3. Exit")
+    print("3. Delete task")
+    print("4. Exit")
 
     choice = input("Choose option: ")
 
     if choice == "1":
-        print(tasks)
+        
+        if len(tasks) == 0:
+            print("No tasks yet!")
+        
+        else :
+            print("\nYour tasks:")
+
+            for index, task in enumerate(tasks, start=1):
+                print(index, "-", task)
+    
 
     elif choice == "2":
         task = input("Enter task: ")
@@ -17,6 +27,24 @@ while True:
         print("Task added!")
 
     elif choice == "3":
+
+        if len(tasks) == 0:
+            print("No tasks to delete!")
+        
+        else:
+            for index, task in enumerate(tasks, start=1):
+                print(index, "-", task)
+
+            task_number = int(input("Enter task number to delete: "))
+
+            if 1 <= task_number <= len(tasks):
+                removed_task = tasks.pop(task_number - 1)
+                print("Deleted:", removed_task)
+
+            else:
+                print("Invalid task number")
+
+    elif choice == "4":
         print("Goodbye!")
         break
 
